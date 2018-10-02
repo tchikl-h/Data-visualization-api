f32f7224b016906bd70b73489600deab4baf287c
Hervé <herve@loukoum.io>
2018-10-02 15:07:11 +0200

    CORS enabled

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,17 @@ import Aggregator from './Aggregator';
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 // send the user to index html page inspite of the url
 app.get('/api/v1/all', (req, res) => {
+  
   res.send('response');
   // let agg = new Aggregator;
   // agg.db.executeQuery('select relation from data;')

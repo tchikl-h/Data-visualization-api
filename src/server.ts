2eb684ea378dfb05bf37a3e2a973dfac5b64b91d
Hervé <herve@loukoum.io>
2018-10-02 14:58:57 +0200

    simple test

--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,13 @@ const app = express();
 
 // send the user to index html page inspite of the url
 app.get('/api/v1/all', (req, res) => {
-  let agg = new Aggregator;
-  agg.db.executeQuery('select relation from data;')
-  .then((response) => {
-    console.log(response);
-    res.send(response);
-  })
+  res.send('response');
+  // let agg = new Aggregator;
+  // agg.db.executeQuery('select relation from data;')
+  // .then((response) => {
+  //   console.log(response);
+  //   res.send(response);
+  // })
 });
 
 app.listen(port);
